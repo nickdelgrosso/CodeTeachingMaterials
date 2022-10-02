854af6d7eff8c6443ef97001ac4910c1abfca53b
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-10-02 15:22:38 +0200

    reorganized cache to match topics directory order, removed order info from notebook filenames

--- utils/make_student_notebook_test.py
+++ utils/make_student_notebook_test.py
@@ -27,7 +27,7 @@ def test_same_result(request, filename):
     verify(
         name=new_filename.name, 
         data=new_filename.read_text(), 
-        directory='./cached' + f'/{request.node.originalname}', 
+        directory='./cached/' + request.node.originalname + '/' + Path(new_filename).parent.name, 
         approve_diff=request.config.option.approve,
         show_diffs=lambda filename1, filename2: void(Popen(["code", '-d', filename1, filename2], print(f"Diffs in:\n{filename1}\n{filename2}\n\n"))),
     )
