3dd46a1eafcddad9f7c867f70bc4dd09ff77612d
nickdelgrosso <delgrosso.nick@gmail.com>
2022-10-06 06:02:54 +0000

    plots

--- utils/merge_requirements.py
+++ utils/merge_requirements.py
@@ -21,5 +21,5 @@ def write_requirements(requirements: Set[str]) -> None:
 if __name__ == '__main__':
     files = get_requirements_files()
     requirements = merge_requirements(files)
-    requirements.update(['jupyterlab', 'jupyterlab-link-share'])
+    requirements.update(['jupyterlab', 'jupyterlab-link-share', 'pytest'])
     write_requirements(requirements)
