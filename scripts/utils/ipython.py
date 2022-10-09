5dfeb8d67ffce508cddd36ef46ad0f77fc56318f
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-10-10 01:58:14 +0200

    ignore seeming error

--- scripts/utils/ipython.py
+++ scripts/utils/ipython.py
@@ -2,6 +2,6 @@
 
 def in_interactive_mode(envs=["ZMQInteractiveShell", "TerminalInteractiveShell"]) -> bool:
     try:
-        return get_ipython().__class__.__name__ in envs
+        return get_ipython().__class__.__name__ in envs  # type: ignore
     except NameError:
         return False
