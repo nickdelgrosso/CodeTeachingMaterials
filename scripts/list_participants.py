382d8a4b49a3b7aad3be34beb62cb4acb38f002d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-10-16 11:50:38 +0200

    clean up

--- scripts/list_participants.py
+++ scripts/list_participants.py
@@ -1,30 +1,30 @@
 import pandas as pd
+
 import zoom_api
 
 
-# Use Case: Get Participation
-from_date = '2022-10-10'
-to_date = '2022-10-14'
-
-creds = zoom_api.Credentials.from_environment_variables()
-oauth = zoom_api.get_server2server_oauth(
-    account_id=creds.zoom_account_id,
-    client_id=creds.zoom_client_id,
-    client_secret=creds.zoom_client_secret,
-)
-token = oauth["access_token"]
-
-user_df = pd.DataFrame(zoom_api.list_users(token=token))
-owner_id = user_df[user_df.role_id == '0'].iloc[-1].id
-print(owner_id)
-
-meetings = pd.DataFrame(zoom_api.list_meetings2(token=token, user_id=owner_id, from_=from_date, to=to_date))
-print(meetings)
-
-unique_meeting_ids = meetings.id.unique()
-print(unique_meeting_ids)
+def list_participants(token, from_date, to_date):
+    user_df = pd.DataFrame(zoom_api.list_users(token=token))
+    owner_id = user_df[user_df.role_id == '0'].iloc[-1].id
+    print(owner_id)
+    meetings = pd.DataFrame(zoom_api.list_meetings2(token=token, user_id=owner_id, from_=from_date, to=to_date))
+    print(meetings)
+    unique_meeting_ids = meetings.id.unique()
+    print(unique_meeting_ids)
+    for meeting_uuid in meetings.uuid[0:1]:
+        print(f'Participants for meeting {meeting_uuid}:')
+        print(pd.DataFrame(zoom_api.get_meeting_participant_events(token=token, meeting_id=meeting_uuid)))
 
 
-for meeting_uuid in meetings.uuid[0:1]:
-    print(f'Participants for meeting {meeting_uuid}:')
-    print(pd.DataFrame(zoom_api.get_meeting_participant_events(token=token, meeting_id=meeting_uuid)))
+if __name__ == '__main__':
+    creds = zoom_api.Credentials.from_environment_variables()
+    oauth = zoom_api.get_server2server_oauth(
+        account_id=creds.zoom_account_id,
+        client_id=creds.zoom_client_id,
+        client_secret=creds.zoom_client_secret,
+    )
+    list_participants(
+        token=(oauth["access_token"]),
+        from_date='2022-10-10',
+        to_date='2022-10-14',
+    )
