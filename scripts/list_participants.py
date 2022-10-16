bae135e6385fd6b0ec81218042380f662ebdf4e9
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-10-16 11:51:44 +0200

    clean up

--- scripts/list_participants.py
+++ scripts/list_participants.py
@@ -6,14 +6,10 @@ import zoom_api
 def list_participants(token, from_date, to_date):
     user_df = pd.DataFrame(zoom_api.list_users(token=token))
     owner_id = user_df[user_df.role_id == '0'].iloc[-1].id
-    print(owner_id)
     meetings = pd.DataFrame(zoom_api.list_meetings2(token=token, user_id=owner_id, from_=from_date, to=to_date))
-    print(meetings)
-    unique_meeting_ids = meetings.id.unique()
-    print(unique_meeting_ids)
     for meeting_uuid in meetings.uuid[0:1]:
         print(f'Participants for meeting {meeting_uuid}:')
-        print(pd.DataFrame(zoom_api.get_meeting_participant_events(token=token, meeting_id=meeting_uuid)))
+        print(pd.DataFrame(zoom_api.get_meeting_participant_events(token=token, meeting_id=meeting_uuid)), end='\n\n')
 
 
 if __name__ == '__main__':
