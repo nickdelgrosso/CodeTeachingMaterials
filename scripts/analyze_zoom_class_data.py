1d9e0c316f7d48f3150b656b3002c8b8453dd427
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2022-12-30 11:10:37 +0100

    calculate folder name once

--- scripts/analyze_zoom_class_data.py
+++ scripts/analyze_zoom_class_data.py
@@ -27,46 +27,44 @@ for teacher in zoom_api.list_users(token=token, role_id=0):
     courses = zoom_api.get_all_meeting_reports(
         token=token, 
         user_id=teacher["id"], 
-        from_=(today - timedelta(days=70)).strftime("%Y-%m-%d"), 
+        from_=(today - timedelta(days=100)).strftime("%Y-%m-%d"), 
         to=today.strftime("%Y-%m-%d"),
     )
     past_meetings.extend(courses)
 
 
+past_meetings = pd.DataFrame(past_meetings)
+
+
+# %% Keep only the Recurring, Fixed Time Meetings (the ones that indicate a course was done)
+past_courses = past_meetings.query('type == 8')
+past_courses
 
 # %%
-len(past_meetings)
+# Make Labels for each course id, for making folder names
+course_labels = {}
+for id, course in past_courses.groupby('id'):
+    topic = course.topic.iloc[0]
+    start_time = parse(course['start_time'].min()).astimezone(pytz.timezone('Europe/Berlin')).strftime('%Y-%m-%d')
+    jsonized_data = json.dumps(course.to_dict('records'), indent=4)
+    label = f"{start_time} {topic} {id}"
+    course_labels[id] = label
+course_labels
 
-# %%
-pd.DataFrame(past_meetings)
 
-# %% [markdown]
-# ### Keep only the Recurring, Fixed Time Meetings (the ones that indicate a course was done)
-
-# %%
-is_recurringfixed = lambda m: zoom_api.meeting_type_is(m, zoom_api.MeetingType.RecurringFixedTime)
-past_courses = pd.DataFrame(list(filter(is_recurringfixed, past_meetings)))
-past_courses.head()
-
-# %% [markdown]
-# ### Group each session into the course it belongs to, then save those sessions to file.
-#  
-# Make two Files:
-#   - One for each session's info
-#   - one for the info that stays consistent across the course (for easier querying)
-
-# %%
+# %% Write Course Info and Session Info
 from pathlib import Path
 folder = Path('data/courses')
 for id, course in past_courses.groupby('id'):
-    topic = course.topic.iloc[0]
-    start_time = parse(course['start_time'].min()).astimezone(pytz.timezone('Europe/Berlin')).strftime('%Y-%m-%d %H:%M')
-    jsonized_data = json.dumps(course.to_dict('records'), indent=4)
-    course_folder = folder / f"{start_time} {topic}"
-    print(course_folder)
+
+    course_folder = folder / course_labels[id]
     course_folder.mkdir(exist_ok=True, parents=True)
+
+    # Write all Course Data to JSON File
+    jsonized_data = json.dumps(course.to_dict('records'), indent=4)
     (course_folder / "sessions.json").write_text(jsonized_data)
 
+    # Write Only the Repeated Course Data to Another File, for easier human reading
     course_info = course[['id', 'type', 'topic', 'user_name', 'user_email', 'source']].iloc[0].to_dict()
     for col in course_info:
         assert len(course[col].unique()) == 1
@@ -74,27 +72,20 @@ for id, course in past_courses.groupby('id'):
 
     
 
-
-# %% [markdown]
-# # Part 2: Get Registrants for each Course
-
-# %%
+# %% Get Registrants for each Course
 for id, course in past_courses.groupby('id'):
-    regs = pd.DataFrame(zoom_api.list_registrants(token=token, meeting_id=course.iloc[0]['id']))
+    regs = pd.DataFrame(zoom_api.list_registrants(token=token, meeting_id=id))
     regs = regs.drop(columns=['address', 'city', 'country', 'zip', 'state', 'phone', 'industry', 'purchasing_time_frame', 'role_in_purchase_process', 'no_of_employees'])
     regs = regs[::-1].reset_index(drop=True) # organize first-to-last to register
     regs.index += 1  # index from 1, to count number of registrants
 
     # Get course folder to save data in
-    topic = course.topic.iloc[0]
-    start_time = parse(course['start_time'].min()).astimezone(pytz.timezone('Europe/Berlin')).strftime('%Y-%m-%d %H:%M')
-    course_folder = folder / f"{start_time} {topic}"
-    print(course_folder)
+    course_folder = folder / course_labels[id]
 
     # Save machine-readable version with data for analysis
     jsonized_data = json.dumps(regs.to_dict('records'), indent=4)
     (course_folder / "registrations.json").write_text(jsonized_data)
-    print('done')
+    
 
     # Save human-readable summary for counting people, communicating with coordinators
     regs2 = pd.DataFrame(index=regs.index).assign(**{
@@ -108,24 +99,12 @@ for id, course in past_courses.groupby('id'):
     # Make copy-pastable version for batch emailing
     mailboxes = [to_mailbox(r['email'], r.get('first_name'), r.get('last_name')) for i, r in regs.iterrows()]
     (course_folder / "registrant_emails.txt").write_text('\n'.join(mailboxes) + '\n')
-
-
-    
-    
     
 
-# %% [markdown]
-# ## Part 3: Get Participation
-
-# %% [markdown]
-# ### Download all Participant Events
-
-# %%
+# %% Get Raw Participation Events Data at Each Session
 for id, course in past_courses.groupby('id'):
-    # Get course folder to save data in
-    topic = course.topic.iloc[0]
-    start_time = parse(course['start_time'].min()).astimezone(pytz.timezone('Europe/Berlin')).strftime('%Y-%m-%d %H:%M')
-    course_folder = folder / f"{start_time} {topic}" / "participation_events"
+    
+    course_folder = folder / course_labels[id] / "participation_events"
     course_folder.mkdir(exist_ok=True, parents=True)
     
     for uuid, session in course.groupby('uuid'):
@@ -135,8 +114,9 @@ for id, course in past_courses.groupby('id'):
             + parse(session['end_time'].iloc[0]).strftime("-%H:%M") \
             + '.json'
 
-        print(course_folder / filename)
-        (course_folder / filename).write_text(json.dumps(parts, indent=4))
+        filepath = course_folder / filename
+        filepath.write_text(json.dumps(parts, indent=4))
+        print(filepath)
 
     
 
@@ -160,3 +140,5 @@ for course_folder in Path('data/courses').glob('*/'):
 
 
 
+
+# %%
